b44e44f9bf6d06e38d2f6675dd154d4033bda3b5
kodjoSoule <souleymanekodjo@gmail.com>
2022-11-01 21:21:08 +0000

    update 11_POO

--- 11_POO/Main.java
+++ 11_POO/Main.java
@@ -3,22 +3,36 @@ public class Main{
         
         Vehicle electric = new ElectricVehicle();
         Vehicle hybrid = new HybridVehicle();
+        Vehicle vehicleKodjo = new HybridVehicle(){
+            @Override
+            public void start(){
+                System.out.println("Starting kodjo's car") ;
+            }
+        } ;
         //Calls
         // Vehicle.start();
         // Vehicle.resource();
 
-        electric.start();
+        //electric.start();
         electric.resource();
+        //vehicleKodjo.start();
 
-        hybrid.start();
+        //hybrid.start();
         hybrid.resource();
+        Color color = Color.Red ;
+        System.out.println("Color : "+ color) ;
+        System.out.println("Color : "+ Color.Blue) ; 
     }
 }
 abstract class Vehicle{
+    public Vehicle(){
+        start() ;
+    }
     public void start(){
         System.out.println("Starting") ;
     }
     abstract void resource();
+    
 }
 class ElectricVehicle extends Vehicle{
     public void resource(){
@@ -29,4 +43,7 @@ class HybridVehicle extends Vehicle{
     public void resource(){
         System.out.println("I use both petrol and electricity") ;
     }
+}
+enum Color {
+    Red,Blue,Black
 }
